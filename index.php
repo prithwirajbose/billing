d6cf50d7fc7f045af7f17e74781409a46d680980
Prithwiraj Bose <sribasu@gmail.com>
2017-11-27 04:08:44 +0530

    initial files

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Billing</title>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<script type="text/javascript" src="/lib/jquery-3.2.1.min.js"></script>
+<link rel="stylesheet" href="/lib/jquery-ui/jquery-ui.min.css" />
+<script type="text/javascript" src="/lib/jquery-ui/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+<div class="content">
+<div class="nav">
+<a href="Invoice">Invoice</a>
+</div>
+<div class="maincontent">Hello World</div></div>
+</body>
+</html>
